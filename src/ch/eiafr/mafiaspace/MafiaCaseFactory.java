509a466866a5d0fb9166e134bcae4dd9b28b17e5
wichtounet <baptistewicht@gmail.com>
2010-12-06 10:43:30 +0100

    Refactor factories

---
+++ src/ch/eiafr/mafiaspace/MafiaCaseFactory.java
@@ -0,0 +1,8 @@
+package ch.eiafr.mafiaspace;
+
+public class MafiaCaseFactory extends CaseFactory {
+	@Override
+    public Case createElement(int type) {
+		throw new UnsupportedOperationException();
+	}
+}
---
+++ src/ch/eiafr/mafiaspace/SpaceCaseFactory.java
@@ -0,0 +1,8 @@
+package ch.eiafr.mafiaspace;
+
+public class SpaceCaseFactory extends CaseFactory {
+    @Override
+    public Case createElement(int type) {
+        return null; 
+    }
+}
