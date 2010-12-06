424e8adf75bef5cc36493819f0718469bd286d69
Buttyx <buttyxavier@gmail.com>
2010-12-06 13:02:26 +0100

    First implementation of GraphicUI

--- src/ch/eiafr/mafiaspace/GraphicUI.java
+++ src/ch/eiafr/mafiaspace/GraphicUI.java
@@ -1,7 +1,27 @@
 package ch.eiafr.mafiaspace;
 
-public class GraphicUI implements WorldObserver {
 
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+public class GraphicUI extends JFrame implements WorldObserver{
+
+    private World worldManager;
+    
+    public GraphicUI(World worldManager)
+    {
+        super("Mafia - Spacelife");
+        this.worldManager = worldManager;
+        this.setSize(new Dimension(800,600));
+        this.setVisible(true);
+        
+    }
+    
+    public void draw()
+    {
+        
+    }
+    
     @Override
     public void worldChanged() {
         throw new UnsupportedOperationException();
@@ -10,5 +30,9 @@ public class GraphicUI implements WorldObserver {
     @Override
     public void worldEnded() {
         throw new UnsupportedOperationException();
+    }
+    
+    public static void main(String[] arg){
+        new GraphicUI(null);
     }
 }
