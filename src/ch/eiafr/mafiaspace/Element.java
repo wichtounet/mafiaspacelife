cff787a9ae257d4a75311f15624de83bb3cda36a
wichtounet <baptistewicht@gmail.com>
2010-12-06 10:49:26 +0100

    Reformat code

--- src/ch/eiafr/mafiaspace/Element.java
+++ src/ch/eiafr/mafiaspace/Element.java
@@ -3,9 +3,9 @@ package ch.eiafr.mafiaspace;
 import java.util.List;
 
 public interface Element {
-	void getName();
-	void getIcon();
-	boolean isAbleToMove();
-	int getPriority();
-	Command getCommand(List<Element> aNeighbors);
+    void getName();
+    void getIcon();
+    boolean isAbleToMove();
+    int getPriority();
+    Command getCommand(List<Element> aNeighbors);
 }
--- src/ch/eiafr/mafiaspace/MafiaCaseFactory.java
+++ src/ch/eiafr/mafiaspace/MafiaCaseFactory.java
@@ -3,6 +3,9 @@ package ch.eiafr.mafiaspace;
 public class MafiaCaseFactory extends CaseFactory {
 	@Override
     public Case createElement(int type) {
-		throw new UnsupportedOperationException();
+        switch(type){
+            case 1:
+
+        }
 	}
 }
