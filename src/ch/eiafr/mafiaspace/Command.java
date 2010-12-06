e6740089fca1668b84855f4f75c51c2cd162a236
wichtounet <baptistewicht@gmail.com>
2010-12-06 11:04:45 +0100

    Continue solve shit

--- src/ch/eiafr/mafiaspace/Command.java
+++ src/ch/eiafr/mafiaspace/Command.java
@@ -2,13 +2,9 @@ package ch.eiafr.mafiaspace;
 
 public class Command {
     public Element _unnamed_Element_;
-<<<<<<< HEAD
     public World _unnamed_World_;
-=======
-    public World   _unnamed_World_;
->>>>>>> ae010237df782749949d85180a114a5eb447c298
 
-    public void do_48() {
+    public void make() {
         throw new UnsupportedOperationException();
     }
 
