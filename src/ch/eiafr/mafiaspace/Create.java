3e29fe01cac8d616163523e989669cced2b01919
kean <kean@mariok08>
2011-01-11 13:52:30 +0100

    added special case: Martian, Kryptonian reproduction

--- src/ch/eiafr/mafiaspace/Create.java
+++ src/ch/eiafr/mafiaspace/Create.java
@@ -1,5 +1,8 @@
 package ch.eiafr.mafiaspace;
 
+import java.util.Collection;
+import java.util.Iterator;
+
 public class Create extends Command {
     Case c;	
 	
@@ -10,12 +13,60 @@ public class Create extends Command {
 	
 	@Override
     public void make() {
-		if(src instanceof Martian || src instanceof Kryptonian) {
-			world.get
-			
+		if(src instanceof Martian) {
+			Case martianCase = getAroundSafeCaseForMartian(c);
+			martianCase.setElement(src);			
+		}
+		else if(src instanceof Kryptonian) {
+			Case kryptonianCase = getAroundSafeCaseForKryptonian(c);
+			kryptonianCase.setElement(src);
 		}
 		else {
 			c.setElement(src);
 		}
     }
+	
+	private Case getAroundSafeCaseForMartian(Case c) {
+		Collection<Case> aroundEmptyCases = world.getPossibleMoves(c);
+		Iterator<Case> itr = aroundEmptyCases.iterator();
+		boolean caseIsSafe = false;;
+		
+		while(itr.hasNext()) {	// iters every empty case around c
+			Case crtCase = itr.next();
+			caseIsSafe = true;
+			for(Case n : world.getNeighbours(crtCase)) {	// iters every neighbour of the case
+				if(	n.getElement() instanceof Kryptonian ||
+					n.getElement() instanceof PlanetKryptonian) {
+					caseIsSafe = false;
+					break;
+				}
+			}
+			if(caseIsSafe)
+				return crtCase;
+		}
+		return null;
+	}
+	
+	private Case getAroundSafeCaseForKryptonian(Case c) {
+		Collection<Case> aroundEmptyCases = world.getPossibleMoves(c);
+		Iterator<Case> itr = aroundEmptyCases.iterator();
+		boolean caseIsSafe = false;;
+		
+		while(itr.hasNext()) {	// iters every empty case around c
+			Case crtCase = itr.next();
+			caseIsSafe = true;
+			for(Case n : world.getNeighbours(crtCase)) {	// iters every neighbour of the case
+				if(	n.getElement() instanceof Martian ||
+					n.getElement() instanceof PlanetMartian) {
+					caseIsSafe = false;
+					break;
+				}
+			}
+			if(caseIsSafe)
+				return crtCase;
+		}
+		return null;
+	}
 }
+
+
