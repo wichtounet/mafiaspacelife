dd96b97c29936ac82dd7261aec0944f0f4a0b0bf
kean <kean@mariok08>
2011-01-20 14:26:09 +0100

    cacca

--- src/ch/eiafr/mafiaspace/SpaceWorldManager.java
+++ src/ch/eiafr/mafiaspace/SpaceWorldManager.java
@@ -5,17 +5,6 @@ public class SpaceWorldManager extends WorldManager {
     protected boolean isWorldEnded() {
         return	isOnlyBlackHoles();
     }
-
-    private boolean isEmpty() {
-    	for (Case[] cases : getWorld().getCases()) {
-            for (Case c : cases) {
-                if (!c.isEmpty()) {
-                	return false;
-                }
-            }
-        }
-    	return true;
-    }
     
     private boolean isOnlyBlackHoles() {
         boolean blackHole = false;
@@ -32,82 +21,6 @@ public class SpaceWorldManager extends WorldManager {
             }
         }
         return blackHole;
-    }
-
-    private boolean isOnlyAsteroid() {
-        boolean asteroid = false;
-
-        for (Case[] cases : getWorld().getCases()) {
-            for (Case c : cases) {
-                if (c.getElement() != null) {
-                    if (c.getElement() instanceof Asteroid) {
-                    	asteroid = true;
-                    } else {
-                        return false;
-                    }
-                }
-            }
-        }
-        return asteroid;
-    }
-    
-    private boolean isOnlyPlanetsAndMartians() {
-        boolean planetsAndMartians = false;
-
-        for (Case[] cases : getWorld().getCases()) {
-            for (Case c : cases) {
-                if (c.getElement() != null) {
-                    if (	c.getElement() instanceof Martian ||
-                    		c.getElement() instanceof PlanetMartian ||
-                    		(c.getElement() instanceof Planet &&
-                    		!(c.getElement() instanceof PlanetKryptonian)) ) {
-                    	planetsAndMartians = true;
-                    } else {
-                        return false;
-                    }
-                }
-            }
-        }
-        return planetsAndMartians;
-    }
-    
-    private boolean isOnlyPlanetsAndKryptonians() {
-        boolean planetsAndKryptonians = false;
-
-        for (Case[] cases : getWorld().getCases()) {
-            for (Case c : cases) {
-                if (c.getElement() != null) {
-                    if (	c.getElement() instanceof Kryptonian ||
-                    		c.getElement() instanceof PlanetKryptonian ||
-                    		(c.getElement() instanceof Planet &&
-                    		!(c.getElement() instanceof PlanetMartian)) ) {
-                    	planetsAndKryptonians = true;
-                    } else {
-                        return false;
-                    }
-                }
-            }
-        }
-        return planetsAndKryptonians;
-    }
-    
-    private boolean isOnlyPlanets() {
-    	boolean planets = false;
-
-        for (Case[] cases : getWorld().getCases()) {
-            for (Case c : cases) {
-                if (c.getElement() != null) {
-                    if ((c.getElement() instanceof Planet &&
-                    	!(c.getElement() instanceof PlanetMartian)) &&
-                    	!(c.getElement() instanceof PlanetKryptonian)) {
-                    	planets = true;
-                    } else {
-                        return false;
-                    }
-                }
-            }
-        }
-        return planets;
     }
 }
 
